dee39a06b40a303680b91ef0cfca7858f3922b9a
Jonyelson Lopes <jonyelsonlopespopos@pop-os.localdomain>
2024-02-23 12:55:34 -0300

    feat: saving the image through the api.

--- src/main/java/com/tecsoftblue/imageliteapi/application/images/ImagesController.java
+++ src/main/java/com/tecsoftblue/imageliteapi/application/images/ImagesController.java
@@ -1,6 +1,11 @@
 package com.tecsoftblue.imageliteapi.application.images;
 
+import com.tecsoftblue.imageliteapi.domain.entity.Image;
+import com.tecsoftblue.imageliteapi.domain.enums.ImageExtension;
+import com.tecsoftblue.imageliteapi.domain.service.IImageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,23 +13,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/images")
 @Slf4j
+@RequiredArgsConstructor
 public class ImagesController {
+
+    private final IImageService service;
 
     @PostMapping
     public ResponseEntity save(
             @RequestParam("file") MultipartFile file,
             @RequestParam("name") String name,
             @RequestParam("tags") List<String> tags
-    ) {
+    ) throws IOException {
 
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        log.info("Nome definido para imagem: {}", name);
-        log.info("Tags: {}", tags);
+
+        Image image = Image.builder()
+                .name(name)
+                .tags(String.join(",", tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+
+        service.save(image);
+
         return ResponseEntity.ok().build();
     }
 }
