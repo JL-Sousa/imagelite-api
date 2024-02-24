2038f7e62f00d1ae1691312585adc8ac45ef4108
Jonyelson Lopes <jonyelsonlopespopos@pop-os.localdomain>
2024-02-23 23:02:31 -0300

    feat: created image url and configured upload size

--- src/main/java/com/tecsoftblue/imageliteapi/application/images/ImagesController.java
+++ src/main/java/com/tecsoftblue/imageliteapi/application/images/ImagesController.java
@@ -1,19 +1,19 @@
 package com.tecsoftblue.imageliteapi.application.images;
 
 import com.tecsoftblue.imageliteapi.domain.entity.Image;
-import com.tecsoftblue.imageliteapi.domain.enums.ImageExtension;
 import com.tecsoftblue.imageliteapi.domain.service.IImageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -23,6 +23,7 @@ import java.util.List;
 public class ImagesController {
 
     private final IImageService service;
+    private final ImageMapper mapper;
 
     @PostMapping
     public ResponseEntity save(
@@ -33,16 +34,18 @@ public class ImagesController {
 
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
 
-        Image image = Image.builder()
-                .name(name)
-                .tags(String.join(",", tags))
-                .size(file.getSize())
-                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
-                .file(file.getBytes())
-                .build();
+        Image image = mapper.mapToImage(file, name, tags);
+        Image savedImage = service.save(image);
+        URI imageUri = buildImageURL(savedImage);
 
-        service.save(image);
+        return ResponseEntity.created(imageUri).build();
+    }
 
-        return ResponseEntity.ok().build();
+    private URI buildImageURL(Image image) {
+        String imagePath = "/" + image.getId();
+        return ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path(imagePath)
+                .build().toUri();
     }
 }
