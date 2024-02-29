f2d181046cc713ca93022e285df4e8ef210e84ab
Jonyelson Lopes <jonyelson20@gmail.com>
2024-02-29 15:04:23 -0300

    feat: implemented the image search endpoint.

--- src/main/java/com/tecsoftblue/imageliteapi/domain/service/IImageService.java
+++ src/main/java/com/tecsoftblue/imageliteapi/domain/service/IImageService.java
@@ -1,7 +1,9 @@
 package com.tecsoftblue.imageliteapi.domain.service;
 
 import com.tecsoftblue.imageliteapi.domain.entity.Image;
+import com.tecsoftblue.imageliteapi.domain.enums.ImageExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface IImageService {
@@ -9,4 +11,6 @@ public interface IImageService {
     Image save(Image image);
 
     Optional<Image> getById(String id);
+
+    List<Image> search(ImageExtension extension, String query);
 }
