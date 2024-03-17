2bbd09dc7c22c415bbfe28871792d4b45ca756cd
Jonyelson Lopes <jonyelson20@gmail.com>
2024-03-16 21:25:34 -0300

    feat: Implemented the authentication feature

---
+++ src/main/java/com/tecsoftblue/imageliteapi/domain/AccessToken.java
@@ -0,0 +1,11 @@
+package com.tecsoftblue.imageliteapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AccessToken {
+
+    private String accessToken;
+}
